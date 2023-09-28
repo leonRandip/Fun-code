3f9cf074869ba5abeb73b7154326c6c9a8bee8b2
Randip Leon <randipleon@Randips-MacBook-Air.local>
2023-09-28 12:24:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+export default function Counter(){
+    
+  const [word,setWord]=useState("a");       
+  
+const toggleFact=()=>{
+  setWord((currentWord) => (currentWord=='a' ? 'The' :"a"));
+};
 
+  return(
+      <button onClick={toggleFact}>
+          Vina is {word} Gay
+      </button>
+  );  
+}
 function App() {
+  let vina=false;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Counter></Counter>
+    </>
+    
   );
 }
 
-export default App;
+
